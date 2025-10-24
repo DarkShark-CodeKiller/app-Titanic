70af667edcbe61a13b7e725271e1c34ca4d6faff
AKACy-bit <2585343854@qq.com>
2025-10-24 23:54:06 +0800

    uplodeapp-titanic

---
+++ app-titanic.py
@@ -0,0 +1,27 @@
+# import packages
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
+
+# show the title
+st.title('Titanic App by Xinyan Li')
+
+# read csv and show the dataframe
+df = pd.read_csv('train.csv')
+st.write(df)
+# create a figure with three subplots, size should be (15, 5)
+# show the box plot for ticket price with different classes
+# you need to set the x labels and y labels
+# a sample diagram is shown below
+fig,ax=plt.subplots(1,3,figsize=(15,5))
+df.Fare[df['Pclass']==1].plot.box(ax=ax[0])
+df.Fare[df['Pclass']==2].plot.box(ax=ax[1])
+df.Fare[df['Pclass']==3].plot.box(ax=ax[2])
+ax[0].set_xlabel('Pclass=1')
+ax[1].set_xlabel('Pclass=2')
+ax[2].set_xlabel('Pclass=3')
+ax[0].set_ylabel('Fare')
+st.pyplot(fig)
